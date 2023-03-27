ae5740720bc5de366ae32d2f4b1ad9fa98c432e4
mattiasduerrmeier <48924241+mattiasduerrmeier@users.noreply.github.com>
2023-03-27 22:02:49 +0200

    Remove class and create user directly instead.

--- typescript-exercise/app.ts
+++ typescript-exercise/app.ts
@@ -1,38 +1,21 @@
 import {User} from "./user";
 
-class ToDoApp {
-    user: User;
-    constructor() {
-        this.user = new User("David", "Gauch", "DavidGauc", "david@example.com", "securepassword123");
-    }
-    start_test(): void {
-        this.user.logIn("DavidGauc", "securepassword123");
-        this.user.createToDo(
-            "Buy t-shirt",
-            "I need a black tshirt from H&M",
-            1,
-            new Date(2023, 19, 3),
-            [""],
-            false
-        );
-        this.user.createToDo(
-            "sysdev assignment 2",
-            "finish assignment 2 for sysdev course",
-            2,
-            new Date(2023, 28, 3),
-            [""],
-            false
-        );
-        this.user.createToDo(
-            "workout",
-            "20 min. running session",
-            3,
-            new Date(2023, 19, 3),
-            [""],
-            false
-        );
-    }
-}
-
-let app = new ToDoApp();
-app.start_test();
+let dc = new User("David", "Gauch", "DavidGauc", "david@example.com", "securepassword123");
+dc.logIn("DavidGauc", "securepassword123");
+dc.createToDo(
+    "Buy t-shirt",
+    "I need a black tshirt from H&M",
+    1,
+    new Date(2023, 19, 3),
+    [""],
+    false
+);
+dc.createToDo(
+    "sysdev assignment 2",
+    "finish assignment 2 for sysdev course",
+    2,
+    new Date(2023, 28, 3),
+    [""],
+    false
+);
+dc.createToDo("workout", "20 min. running session", 3, new Date(2023, 19, 3), [""], false);
