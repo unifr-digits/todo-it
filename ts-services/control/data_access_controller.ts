68c64de089725d7fb8c270230af7fa048614e69b
DavidGauc <david.gauch@unifr.ch>
2023-05-17 17:20:42 +0200

    added creation of team table

--- ts-services/control/data_access_controller.ts
+++ ts-services/control/data_access_controller.ts
@@ -19,8 +19,9 @@ export class DataAccessController {
     static async createDataSchema() : Promise<void> {
         let statements = [
             "CREATE TABLE IF NOT EXISTS users (user_id serial, email text UNIQUE, name text, password_hash text, PRIMARY KEY (user_id))",
-            "CREATE TABLE IF NOT EXISTS tasks (task_id uuid, name text, done bool, user_id int, PRIMARY KEY (task_id), FOREIGN KEY (user_id) REFERENCES users(user_id))",
-            "CREATE TABLE IF NOT EXISTS projects (project_id uuid, name text, user_id int, PRIMARY KEY (project_id), FOREIGN KEY (user_id) REFERENCES users(user_id))"
+            "CREATE TABLE IF NOT EXISTS tasks (task_id text, name text, done bool, user_id int, PRIMARY KEY (task_id), FOREIGN KEY (user_id) REFERENCES users(user_id))",
+            "CREATE TABLE IF NOT EXISTS projects (project_id text, name text, user_id int, PRIMARY KEY (project_id), FOREIGN KEY (user_id) REFERENCES users(user_id))",
+            "CREATE TABLE IF NOT EXISTS teams (team_id text, name text,description text, user_id int, PRIMARY KEY (team_id), FOREIGN KEY (user_id) REFERENCES users(user_id))"
         ];
         for (let s of statements) {
             let results = await DataAccessController.pool.query(s);
